3d390c6e9c91dd6cee6500ae871af07e1c2c85b6
Carter Black <carterb@mailbox.org>
2020-11-04 09:54:19 -0800

    deploy

--- utils/auth/firebase-admin.ts
+++ utils/auth/firebase-admin.ts
@@ -1,12 +1,20 @@
 import * as admin from "firebase-admin";
-import serviceAccount from "./firebase.json";
+
+const project_id = process.env.PROJECT_ID;
+const private_key = process.env.PRIVATE_KEY?.replace(/\\n/g, "\n");
+const client_email = process.env.CLIENT_EMAIL;
+
+if (!project_id || !private_key || !client_email)
+  throw Error(
+    "Can not see Firebase Environment Variables. Did you load them correctly?"
+  );
 
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
-      privateKey: serviceAccount.private_key,
-      clientEmail: serviceAccount.client_email,
-      projectId: serviceAccount.project_id,
+      privateKey: private_key,
+      clientEmail: client_email,
+      projectId: project_id,
     }),
     databaseURL: "https://ferrisbot-6e0f1.firebaseio.com/",
   });
