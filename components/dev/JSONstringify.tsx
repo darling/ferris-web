265cec7846139c89e5d0980308ba27483d4c2a34
Carter Black <carterb@mailbox.org>
2020-11-09 23:06:58 -0800

    color change

--- components/dev/JSONstringify.tsx
+++ components/dev/JSONstringify.tsx
@@ -1,13 +1,13 @@
 interface Props {
-  data: any;
+	data: any;
 }
 
 const JSONstringify = ({ data }: Props) => {
-  return (
-    <pre className="bg-blue-900 rounded-lg p-5">
-      <code>{"DEBUG\n" + JSON.stringify(data, null, 4)}</code>
-    </pre>
-  );
+	return (
+		<pre className="bg-green-900 rounded-lg p-5">
+			<code>{'DEBUG\n' + JSON.stringify(data, null, 4)}</code>
+		</pre>
+	);
 };
 
 export default JSONstringify;
