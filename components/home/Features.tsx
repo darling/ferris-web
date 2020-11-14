252a0a59008062c62a2085313b3543eed4d7046f
Carter Black <carterb@mailbox.org>
2020-11-14 13:41:54 -0800

    front page button fix

--- components/home/Features.tsx
+++ components/home/Features.tsx
@@ -1,124 +1,132 @@
+import Link from 'next/link';
+
 export const HomeFeatures = () => {
-  return (
-    <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
-      <div className="flex flex-col lg:flex-row">
-        <div className="max-w-xl pr-16 mx-auto mb-10">
-          <h5 className="mb-6 text-3xl font-extrabold leading-none text-green-300">
-            Use a bot that works.
-          </h5>
-          <p className="mb-6">
-            Build a server with the best utilites availible. From a real-time
-            web panel to rich and improved commands that will protect your
-            community.
-          </p>
-          <div className="flex items-center">
-            <button
-              type="submit"
-              className="inline-flex items-center justify-center h-12 px-6 mr-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-green-400 hover:bg-green-700 focus:shadow-outline focus:outline-none"
-            >
-              Get started
-            </button>
-            <a
-              href="/"
-              aria-label=""
-              className="inline-flex items-center font-semibold transition-colors duration-200 text-green-400 hover:text-green-800"
-            >
-              Learn more
-            </a>
-          </div>
-        </div>
-        <div className="grid gap-5 row-gap-5 sm:grid-cols-2">
-          <div className="max-w-md">
-            <div className="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-gray-800">
-              <svg
-                className="w-8 h-8 text-green-300"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M2.166 4.999A11.954 11.954 0 0010 1.944 11.954 11.954 0 0017.834 5c.11.65.166 1.32.166 2.001 0 5.225-3.34 9.67-8 11.317C5.34 16.67 2 12.225 2 7c0-.682.057-1.35.166-2.001zm11.541 3.708a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </div>
-            <h6 className="mb-2 font-semibold leading-5">
-              Security from the start
-            </h6>
-            <p className="text-sm text-green-100">
-              Ferris has been developed with security in mind, using Ferris is
-              easy, intuitive, and powerful. The bot gives your mods a list of
-              tools that work great.
-            </p>
-          </div>
-          <div className="max-w-md">
-            <div className="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-gray-800">
-              <svg
-                className="w-8 h-8 text-green-300"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path d="M3 12v3c0 1.657 3.134 3 7 3s7-1.343 7-3v-3c0 1.657-3.134 3-7 3s-7-1.343-7-3z" />
-                <path d="M3 7v3c0 1.657 3.134 3 7 3s7-1.343 7-3V7c0 1.657-3.134 3-7 3S3 8.657 3 7z" />
-                <path d="M17 5c0 1.657-3.134 3-7 3S3 6.657 3 5s3.134-3 7-3 7 1.343 7 3z" />
-              </svg>
-            </div>
-            <h6 className="mb-2 font-semibold leading-5">Real time data</h6>
-            <p className="text-sm text-green-100">
-              Discord offers real-time chat, it's time that bots keep up. Ferris
-              uses a database configured to send data in less than a
-              micro-second.
-            </p>
-          </div>
-          <div className="max-w-md">
-            <div className="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-gray-800">
-              <svg
-                className="w-8 h-8 text-green-300"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M2 5a2 2 0 012-2h12a2 2 0 012 2v2a2 2 0 01-2 2H4a2 2 0 01-2-2V5zm14 1a1 1 0 11-2 0 1 1 0 012 0zM2 13a2 2 0 012-2h12a2 2 0 012 2v2a2 2 0 01-2 2H4a2 2 0 01-2-2v-2zm14 1a1 1 0 11-2 0 1 1 0 012 0z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </div>
-            <h6 className="mb-2 font-semibold leading-5">Build a platform</h6>
-            <p className="text-sm text-green-100">
-              Use Ferris to run social events, control giveaways, help announce
-              messages. Giving utilities to you through chat allows you to work
-              on building a better community.
-            </p>
-          </div>
-          <div className="max-w-md">
-            <div className="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-gray-800">
-              <svg
-                className="w-8 h-8 text-green-300"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M12.316 3.051a1 1 0 01.633 1.265l-4 12a1 1 0 11-1.898-.632l4-12a1 1 0 011.265-.633zM5.707 6.293a1 1 0 010 1.414L3.414 10l2.293 2.293a1 1 0 11-1.414 1.414l-3-3a1 1 0 010-1.414l3-3a1 1 0 011.414 0zm8.586 0a1 1 0 011.414 0l3 3a1 1 0 010 1.414l-3 3a1 1 0 11-1.414-1.414L16.586 10l-2.293-2.293a1 1 0 010-1.414z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </div>
-            <h6 className="mb-2 font-semibold leading-5">
-              Responsive Developers
-            </h6>
-            <p className="text-sm text-green-100">
-              Join the Support Discord to contact the developers of Ferris. Get
-              the features you want, made for you, in your server.
-            </p>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+			<div className="flex flex-col lg:flex-row">
+				<div className="max-w-xl pr-16 mx-auto mb-10">
+					<h5 className="mb-6 text-3xl font-extrabold leading-none text-green-300">
+						Use a bot that works.
+					</h5>
+					<p className="mb-6">
+						Build a server with the best utilites availible. From a
+						real-time web panel to rich and improved commands that
+						will protect your community.
+					</p>
+					<div className="flex items-center">
+						<Link href="/discord">
+							<button
+								type="submit"
+								className="inline-flex items-center justify-center h-12 px-6 mr-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-green-400 hover:bg-green-700 focus:shadow-outline focus:outline-none"
+							>
+								Get started
+							</button>
+						</Link>
+						<Link href="/docs">
+							<a className="inline-flex items-center font-semibold transition-colors duration-200 text-green-400 hover:text-green-800">
+								Learn more
+							</a>
+						</Link>
+					</div>
+				</div>
+				<div className="grid gap-5 row-gap-5 sm:grid-cols-2">
+					<div className="max-w-md">
+						<div className="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-gray-800">
+							<svg
+								className="w-8 h-8 text-green-300"
+								fill="currentColor"
+								viewBox="0 0 20 20"
+								xmlns="http://www.w3.org/2000/svg"
+							>
+								<path
+									fillRule="evenodd"
+									d="M2.166 4.999A11.954 11.954 0 0010 1.944 11.954 11.954 0 0017.834 5c.11.65.166 1.32.166 2.001 0 5.225-3.34 9.67-8 11.317C5.34 16.67 2 12.225 2 7c0-.682.057-1.35.166-2.001zm11.541 3.708a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+									clipRule="evenodd"
+								/>
+							</svg>
+						</div>
+						<h6 className="mb-2 font-semibold leading-5">
+							Security from the start
+						</h6>
+						<p className="text-sm text-green-100">
+							Ferris has been developed with security in mind,
+							using Ferris is easy, intuitive, and powerful. The
+							bot gives your mods a list of tools that work great.
+						</p>
+					</div>
+					<div className="max-w-md">
+						<div className="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-gray-800">
+							<svg
+								className="w-8 h-8 text-green-300"
+								fill="currentColor"
+								viewBox="0 0 20 20"
+								xmlns="http://www.w3.org/2000/svg"
+							>
+								<path d="M3 12v3c0 1.657 3.134 3 7 3s7-1.343 7-3v-3c0 1.657-3.134 3-7 3s-7-1.343-7-3z" />
+								<path d="M3 7v3c0 1.657 3.134 3 7 3s7-1.343 7-3V7c0 1.657-3.134 3-7 3S3 8.657 3 7z" />
+								<path d="M17 5c0 1.657-3.134 3-7 3S3 6.657 3 5s3.134-3 7-3 7 1.343 7 3z" />
+							</svg>
+						</div>
+						<h6 className="mb-2 font-semibold leading-5">
+							Real time data
+						</h6>
+						<p className="text-sm text-green-100">
+							Discord offers real-time chat, it's time that bots
+							keep up. Ferris uses a database configured to send
+							data in less than a micro-second.
+						</p>
+					</div>
+					<div className="max-w-md">
+						<div className="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-gray-800">
+							<svg
+								className="w-8 h-8 text-green-300"
+								fill="currentColor"
+								viewBox="0 0 20 20"
+								xmlns="http://www.w3.org/2000/svg"
+							>
+								<path
+									fillRule="evenodd"
+									d="M2 5a2 2 0 012-2h12a2 2 0 012 2v2a2 2 0 01-2 2H4a2 2 0 01-2-2V5zm14 1a1 1 0 11-2 0 1 1 0 012 0zM2 13a2 2 0 012-2h12a2 2 0 012 2v2a2 2 0 01-2 2H4a2 2 0 01-2-2v-2zm14 1a1 1 0 11-2 0 1 1 0 012 0z"
+									clipRule="evenodd"
+								/>
+							</svg>
+						</div>
+						<h6 className="mb-2 font-semibold leading-5">
+							Build a platform
+						</h6>
+						<p className="text-sm text-green-100">
+							Use Ferris to run social events, control giveaways,
+							help announce messages. Giving utilities to you
+							through chat allows you to work on building a better
+							community.
+						</p>
+					</div>
+					<div className="max-w-md">
+						<div className="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-gray-800">
+							<svg
+								className="w-8 h-8 text-green-300"
+								fill="currentColor"
+								viewBox="0 0 20 20"
+								xmlns="http://www.w3.org/2000/svg"
+							>
+								<path
+									fillRule="evenodd"
+									d="M12.316 3.051a1 1 0 01.633 1.265l-4 12a1 1 0 11-1.898-.632l4-12a1 1 0 011.265-.633zM5.707 6.293a1 1 0 010 1.414L3.414 10l2.293 2.293a1 1 0 11-1.414 1.414l-3-3a1 1 0 010-1.414l3-3a1 1 0 011.414 0zm8.586 0a1 1 0 011.414 0l3 3a1 1 0 010 1.414l-3 3a1 1 0 11-1.414-1.414L16.586 10l-2.293-2.293a1 1 0 010-1.414z"
+									clipRule="evenodd"
+								/>
+							</svg>
+						</div>
+						<h6 className="mb-2 font-semibold leading-5">
+							Responsive Developers
+						</h6>
+						<p className="text-sm text-green-100">
+							Join the Support Discord to contact the developers
+							of Ferris. Get the features you want, made for you,
+							in your server.
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
--- components/home/Heros.tsx
+++ components/home/Heros.tsx
@@ -1,113 +1,118 @@
-import Link from "next/link";
+import Link from 'next/link';
 
 export const HomeHero = () => {
-  return (
-    <div className="relative flex flex-col-reverse px-4 p-8 md:p-16 mx-auto lg:block lg:flex-col lg:py-32 xl:py-48 md:px-8 sm:max-w-xl md:max-w-full">
-      <div className="z-0 flex justify-center h-full overflow-hidden lg:pt-24 lg:pb-16 lg:pr-8 xl:pr-0 lg:w-1/2 lg:absolute lg:justify-end lg:bottom-0 lg:left-0 lg:items-center">
-        <img
-          src="https://i.imgur.com/mEbkkA2.png"
-          className="object-right hidden md:flex lg:w-auto lg:h-full"
-          alt=""
-        />
-      </div>
-      <div className="relative flex justify-end max-w-xl mx-auto lg:max-w-screen-xl">
-        <div className="lg:pr-5 lg:max-w-lg lg:mb-0">
-          <div className="max-w-xl mb-6">
-            <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold text-gray-100 sm:text-4xl sm:leading-none">
-              Let Ferris protect your <br className="hidden md:block" />{" "}
-              <span className="inline-block text-green-400">community</span>.
-            </h2>
-            <p className="text-base text-green-100 md:text-lg">
-              Ferris is a discord bot created to secure and manage Discord
-              servers of all sizes.
-            </p>
-          </div>
-          <form>
-            <div className="flex items-center mt-4">
-              <button
-                type="submit"
-                className="inline-flex items-center justify-center h-12 px-6 mr-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-green-400 hover:bg-green-700 focus:shadow-outline focus:outline-none"
-              >
-                Get Started
-              </button>
-              <a
-                href="/"
-                aria-label=""
-                className="inline-flex items-center font-semibold transition-colors duration-200 text-green-400 hover:text-deep-purple-800"
-              >
-                Learn more
-              </a>
-            </div>
-          </form>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="relative flex flex-col-reverse px-4 p-8 md:p-16 mx-auto lg:block lg:flex-col lg:py-32 xl:py-48 md:px-8 sm:max-w-xl md:max-w-full">
+			<div className="z-0 flex justify-center h-full overflow-hidden lg:pt-24 lg:pb-16 lg:pr-8 xl:pr-0 lg:w-1/2 lg:absolute lg:justify-end lg:bottom-0 lg:left-0 lg:items-center">
+				<img
+					src="https://i.imgur.com/mEbkkA2.png"
+					className="object-right hidden md:flex lg:w-auto lg:h-full"
+					alt=""
+				/>
+			</div>
+			<div className="relative flex justify-end max-w-xl mx-auto lg:max-w-screen-xl">
+				<div className="lg:pr-5 lg:max-w-lg lg:mb-0">
+					<div className="max-w-xl mb-6">
+						<h2 className="max-w-lg mb-6 font-sans text-3xl font-bold text-gray-100 sm:text-4xl sm:leading-none">
+							Let Ferris protect your{' '}
+							<br className="hidden md:block" />{' '}
+							<span className="inline-block text-green-400">
+								community
+							</span>
+							.
+						</h2>
+						<p className="text-base text-green-100 md:text-lg">
+							Ferris is a discord bot created to secure and manage
+							Discord servers of all sizes.
+						</p>
+					</div>
+					<form>
+						<div className="flex items-center mt-4">
+							<Link href="/api/login">
+								<button
+									type="submit"
+									className="inline-flex items-center justify-center h-12 px-6 mr-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-green-400 hover:bg-green-700 focus:shadow-outline focus:outline-none"
+								>
+									Get started
+								</button>
+							</Link>
+							<Link href="/docs">
+								<a className="inline-flex items-center font-semibold transition-colors duration-200 text-green-400 hover:text-green-800">
+									Learn more
+								</a>
+							</Link>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export const HomeEndHero = () => {
-  return (
-    <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
-      <div className="max-w-xl sm:mx-auto lg:max-w-2xl">
-        <div className="flex flex-col mb-16 sm:text-center sm:mb-0">
-          <Link href="/">
-            <a className="mb-6 sm:mx-auto">
-              <div className="flex items-center justify-center w-12 h-12 rounded-full bg-gray-800">
-                <svg
-                  className="w-10 h-10 text-green-100"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-                </svg>
-              </div>
-            </a>
-          </Link>
-          <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
-            <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-green-100 sm:text-4xl md:mx-auto">
-              <span className="relative inline-block">
-                <svg
-                  viewBox="0 0 52 24"
-                  fill="currentColor"
-                  className="absolute top-0 left-0 z-0 hidden w-32 -mt-8 -ml-20 text-green-100 lg:w-32 lg:-ml-28 lg:-mt-10 sm:block"
-                >
-                  <defs>
-                    <pattern
-                      id="e77df901-b9d7-4b9b-822e-16b2d410795b"
-                      x="0"
-                      y="0"
-                      width=".135"
-                      height=".30"
-                    >
-                      <circle cx="1" cy="1" r=".7" />
-                    </pattern>
-                  </defs>
-                  <rect
-                    fill="url(#e77df901-b9d7-4b9b-822e-16b2d410795b)"
-                    width="52"
-                    height="24"
-                  />
-                </svg>
-                <span className="relative">Still</span>
-              </span>{" "}
-              have questions?
-            </h2>
-            <p className="text-base text-green-100 md:text-lg">
-              Try Ferris for free, if you don't like it, don't use it! Ferris is
-              under development, so please make sure to check out the Support
-              Server to input constructive critisizm or suggest new features.
-            </p>
-          </div>
-          <div>
-            <Link href="/">
-              <a className="inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md hover:shadow-xl bg-green-400 hover:bg-green-700 focus:shadow-xl focus:outline-none">
-                Get started
-              </a>
-            </Link>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+			<div className="max-w-xl sm:mx-auto lg:max-w-2xl">
+				<div className="flex flex-col mb-16 sm:text-center sm:mb-0">
+					<Link href="/">
+						<a className="mb-6 sm:mx-auto">
+							<div className="flex items-center justify-center w-12 h-12 rounded-full bg-gray-800">
+								<svg
+									className="w-10 h-10 text-green-100"
+									fill="currentColor"
+									viewBox="0 0 20 20"
+									xmlns="http://www.w3.org/2000/svg"
+								>
+									<path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+								</svg>
+							</div>
+						</a>
+					</Link>
+					<div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
+						<h2 className="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-green-100 sm:text-4xl md:mx-auto">
+							<span className="relative inline-block">
+								<svg
+									viewBox="0 0 52 24"
+									fill="currentColor"
+									className="absolute top-0 left-0 z-0 hidden w-32 -mt-8 -ml-20 text-green-100 lg:w-32 lg:-ml-28 lg:-mt-10 sm:block"
+								>
+									<defs>
+										<pattern
+											id="e77df901-b9d7-4b9b-822e-16b2d410795b"
+											x="0"
+											y="0"
+											width=".135"
+											height=".30"
+										>
+											<circle cx="1" cy="1" r=".7" />
+										</pattern>
+									</defs>
+									<rect
+										fill="url(#e77df901-b9d7-4b9b-822e-16b2d410795b)"
+										width="52"
+										height="24"
+									/>
+								</svg>
+								<span className="relative">Still</span>
+							</span>{' '}
+							have questions?
+						</h2>
+						<p className="text-base text-green-100 md:text-lg">
+							Try Ferris for free, if you don't like it, don't use
+							it! Ferris is under development, so please make sure
+							to check out the Support Server to input
+							constructive critisizm or suggest new features.
+						</p>
+					</div>
+					<div>
+						<Link href="/api/login">
+							<a className="inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md hover:shadow-xl bg-green-400 hover:bg-green-700 focus:shadow-xl focus:outline-none">
+								Get started
+							</a>
+						</Link>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
