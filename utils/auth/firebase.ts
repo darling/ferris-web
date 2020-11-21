5a000f4105069dc2c283ee3cab0b1dbb3a5e6ab3
Carter Black <carterb@mailbox.org>
2020-11-20 23:15:21 -0800

    firebase branching

--- utils/auth/firebase.ts
+++ utils/auth/firebase.ts
@@ -1,16 +1,18 @@
-import firebase from "firebase";
+import firebase from 'firebase/app';
+import 'firebase/database';
+import 'firebase/auth';
 
 const config = {
-  apiKey: "AIzaSyDeeEzsuPxJOv_JR9WkjzT58Dsp38pWCjM",
-  authDomain: "ferrisbot-6e0f1.firebaseapp.com",
-  databaseURL: "https://ferrisbot-6e0f1.firebaseio.com",
-  projectId: "ferrisbot-6e0f1",
-  appId: "1:479457116766:web:56a4445e135279d5fbeacf",
-  measurementId: "G-B6LVJ99H13",
+	apiKey: 'AIzaSyDeeEzsuPxJOv_JR9WkjzT58Dsp38pWCjM',
+	authDomain: 'ferrisbot-6e0f1.firebaseapp.com',
+	databaseURL: 'https://ferrisbot-6e0f1.firebaseio.com',
+	projectId: 'ferrisbot-6e0f1',
+	appId: '1:479457116766:web:56a4445e135279d5fbeacf',
+	measurementId: 'G-B6LVJ99H13',
 };
 
 if (!firebase.apps.length) {
-  firebase.initializeApp(config);
+	firebase.initializeApp(config);
 }
 
 const app = firebase;
