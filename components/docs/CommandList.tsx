44693fb31f5872fa372dbd72050c14c8bb134935
Carter Black <carterb@mailbox.org>
2020-12-25 01:30:51 -0800

    updated command list

--- components/docs/CommandList.tsx
+++ components/docs/CommandList.tsx
@@ -58,6 +58,117 @@ const commands: ICommandList = {
 				},
 			],
 		},
+		{
+			name: 'invite',
+			description: 'Invite Ferris to your own Discord Server.',
+		},
+		{
+			name: 'ping',
+			description: 'Check if Ferris can respond and see your message.',
+		},
+		{
+			name: 'privacy',
+			description: 'Get the bots privacy policy.',
+		},
+		{
+			name: 'createrole',
+			description: 'Create a role that has no permissions.',
+			args: [
+				{
+					name: 'name',
+					type: 'string',
+					required: true,
+					description:
+						'The name of the role you want to make. No spaces.',
+				},
+				{
+					name: 'color',
+					type: 'string',
+					required: false,
+					description:
+						'The Hex value of the color. Ideally it would be something like "FFFFFF" but you can try other options.',
+				},
+			],
+		},
+		{
+			name: 'deleterole',
+			description: 'Delete a role.',
+			args: [
+				{
+					name: 'role',
+					type: 'role',
+					required: true,
+					description: 'The mention or ID of the role to be deleted',
+				},
+			],
+		},
+		{
+			name: 'setrole',
+			description: 'Give a role to a member.',
+			args: [
+				{
+					name: 'member',
+					type: 'member',
+					required: true,
+					description: 'The mention or ID of the member.',
+				},
+				{
+					name: 'role',
+					type: 'role',
+					required: true,
+					description: 'The mention or ID of the role.',
+				},
+			],
+		},
+		{
+			name: 'removerole',
+			description: 'Remove a role from a member.',
+			args: [
+				{
+					name: 'member',
+					type: 'member',
+					required: true,
+					description: 'The mention or ID of the member.',
+				},
+				{
+					name: 'role',
+					type: 'role',
+					required: true,
+					description: 'The mention or ID of the role.',
+				},
+			],
+		},
+		{
+			name: 'hoistrole',
+			description: 'Toggle the hoist property of any managable role.',
+			args: [
+				{
+					name: 'role',
+					type: 'role',
+					required: true,
+					description: 'The mention or ID of the role.',
+				},
+			],
+		},
+		{
+			name: 'rolecolor',
+			description: 'Edit a roles color.',
+			args: [
+				{
+					name: 'role',
+					type: 'role',
+					required: true,
+					description: 'The mention or ID of the role to be edited.',
+				},
+				{
+					name: 'color',
+					type: 'string',
+					required: false,
+					description:
+						'The Hex value of the color. Ideally it would be something like "FFFFFF" but you can try other options.',
+				},
+			],
+		},
 	],
 	moderation: [
 		{
@@ -95,6 +206,158 @@ const commands: ICommandList = {
 					required: false,
 					description:
 						"The reason that will be put for the kick. If given nothing just the user's id will be stored in the audit log.",
+				},
+			],
+		},
+		{
+			name: 'purge',
+			description: 'Remove a given amount of messages.',
+			args: [
+				{
+					name: 'amount',
+					type: 'number',
+					required: true,
+					description:
+						'The amount of messages to be removed. Ideally it would be something like 20 or such, but can be adjusted.',
+				},
+			],
+		},
+		{
+			name: 'mute',
+			description: 'Mute a user by giving them an assigned role.',
+			args: [
+				{
+					name: 'member',
+					type: 'member',
+					required: true,
+					description: 'The member to be muted.',
+				},
+				{
+					name: 'time',
+					type: 'duration',
+					required: true,
+					description:
+						'The amount of time. An example could be something like "2h"',
+				},
+				{
+					name: 'reason',
+					type: '...string',
+					required: false,
+					description:
+						"Given reason for muting a member. If no reason given the user's id will be used.",
+				},
+			],
+		},
+		{
+			name: 'muterole',
+			description: 'Assign the role used to mute members.',
+			args: [
+				{
+					name: 'role',
+					type: 'role',
+					required: false,
+					description:
+						'The mention or ID of the role. If no given role, the mute role will be removed.',
+				},
+			],
+		},
+		{
+			name: 'unmute',
+			description: 'Unmute a member.',
+			args: [
+				{
+					name: 'member',
+					type: 'member',
+					required: true,
+					description:
+						'Unmutes a member from mention or ID. This will close the case and remove the role, not just remove the role.',
+				},
+			],
+		},
+		{
+			name: 'warn',
+			description: 'Warn a member.',
+			args: [
+				{
+					name: 'member',
+					type: 'member',
+					required: true,
+					description: 'Mention or ID of member to warn',
+				},
+				{
+					name: 'reason',
+					type: '...string',
+					required: false,
+					description:
+						"Given reason for muting a member. If no reason given the user's id will be used.",
+				},
+			],
+		},
+		{
+			name: 'listwarns',
+			description: 'List a users infractions.',
+			args: [
+				{
+					name: 'member',
+					type: 'member',
+					required: true,
+					description: 'Mention or ID of member',
+				},
+			],
+		},
+		{
+			name: 'delwarn',
+			description: 'List a users infractions.',
+			args: [
+				{
+					name: 'member',
+					type: 'member',
+					required: true,
+					description: 'Mention or ID of member',
+				},
+				{
+					name: 'warning',
+					type: 'number',
+					required: true,
+					description:
+						'The number gotten from listwarns command to delete.',
+				},
+			],
+		},
+	],
+	logging: [
+		{
+			name: 'setlogchannel',
+			description: 'Set the channel used to log.',
+			args: [
+				{
+					name: 'channel',
+					type: 'textchannel',
+					required: true,
+					description: 'Mention or ID of channel',
+				},
+			],
+		},
+		{
+			name: 'stoplogging',
+			description: 'Temporarily stop logging.',
+		},
+		{
+			name: 'startlogging',
+			description: 'Start logging.',
+		},
+	],
+	configuration: [
+		{
+			name: 'setprefix',
+			description: 'Set/get the prefix for your server.',
+			args: [
+				{
+					name: 'prefix',
+					type: 'string',
+					required: false,
+					description:
+						'Put the prefix you would like to change to. No spaces.',
 				},
 			],
 		},
