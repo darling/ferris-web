37c906ecda75c6a709da836bcf9a31b403811bcb
Carter Black <carterb@mailbox.org>
2021-05-27 02:22:01 -0700

    permissions updated to 8

--- pages/add.tsx
+++ pages/add.tsx
@@ -4,10 +4,14 @@ import Layout from '../components/Layout';
 const Discord = () => {
 	useEffect(() => {
 		window.location.replace(
-			'https://discord.com/oauth2/authorize?client_id=637804742935838751&permissions=2134207679&scope=bot'
+			'https://discord.com/oauth2/authorize?client_id=637804742935838751&permissions=8&scope=bot'
 		);
 	});
-	return <Layout></Layout>;
+	return (
+		<Layout>
+			https://discord.com/oauth2/authorize?client_id=637804742935838751&permissions=8&scope=bot
+		</Layout>
+	);
 };
 
 export default Discord;
