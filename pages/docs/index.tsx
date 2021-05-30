6b59e093e778de9c9ad7eda4b256eaba9b42d461
Carter Black <carterb@mailbox.org>
2021-05-30 12:09:49 -0700

    commands page context

--- pages/docs/index.tsx
+++ pages/docs/index.tsx
@@ -18,6 +18,7 @@ const actions = [
 		icon: BookOpenIcon,
 		iconForeground: 'text-indigo-700',
 		iconBackground: 'bg-indigo-50',
+		desc: 'Stuck setting up our great features? Don’t worry, our guide will show you how to setup a specific command!',
 	},
 	{
 		title: 'Getting Started',
@@ -25,6 +26,7 @@ const actions = [
 		icon: PlusCircleIcon,
 		iconForeground: 'text-green-700',
 		iconBackground: 'bg-green-50',
+		desc: 'Our beginners guide will teach you all about Ferris’ rocky life!',
 	},
 	{
 		title: 'Control Panel',
@@ -32,6 +34,7 @@ const actions = [
 		icon: TerminalIcon,
 		iconForeground: 'text-red-700',
 		iconBackground: 'bg-red-50',
+		desc: "If you’re looking to take over the world... oh? Starting small? Then, your server, click here to access Ferris' best features the easy way.",
 	},
 	{
 		title: 'Discord Server',
@@ -39,6 +42,7 @@ const actions = [
 		icon: ChatIcon,
 		iconForeground: 'text-blue-700',
 		iconBackground: 'bg-blue-50',
+		desc: 'Help support Ferris and become a sturdy member of the family by joining the official Ferris Discord Server!',
 	},
 ];
 
@@ -105,7 +109,7 @@ const DocsHomeSidebar: FC = () => {
 									<path d="M20 4h1a1 1 0 00-1-1v1zm-1 12a1 1 0 102 0h-2zM8 3a1 1 0 000 2V3zM3.293 19.293a1 1 0 101.414 1.414l-1.414-1.414zM19 4v12h2V4h-2zm1-1H8v2h12V3zm-.707.293l-16 16 1.414 1.414 16-16-1.414-1.414z" />
 								</svg>
 							</span>
-							This is the link description
+							{action.desc}
 						</div>
 					</Link>
 				))}
@@ -116,7 +120,7 @@ const DocsHomeSidebar: FC = () => {
 
 const DocsHome: FC = () => {
 	const data =
-		"## Brief\n\nWelcome to Ferris Documentation Home. This page is for users looking for information on how to use the Ferris Documentation bot. For more information on what the bot does and why it's useful, please visit our [Home Page](/). \n\nIf you're still having trouble understanding this process, we have a troubleshooting section at the bottom of this page that may help!\n\n## Getting Started\n\nCheck out the [Getting Started](/docs/getting-started) guide in order to get started with Ferris.\n\n## Commands\n\nIf you want to be able to see the list of commands we offer, please check out the [Commands Page](/docs/commands)\n\n## Troubleshooting\n\nIn order to get the best support, please join the Discord Server and ask for help in the #help channel.\n\n- [Join Discord Server](/discord) \n\n## Thank you\n\n- [Support Ferris on Patreon](/patreon) \n\n- [Join Discord Server](/discord)\n\nThank you for taking the time to learn about how to use Ferris. We hope it helps your community!";
+		"## Brief\n\nWelcome to Ferris Documentation Home. This page is for users looking for information on how to use the Ferris Documentation bot. For more information on what the bot does and why it's useful, please visit our [Home Page](/). \n\nIf you're still having trouble understanding this process, we have a troubleshooting section at the bottom of this page that may help!\n\n## Getting Started\n\nCheck out the [Getting Started](/docs/getting-started) guide in order to get started with Ferris.\n\n## Commands\n\nIf you want to be able to see the list of commands we offer, please check out the [Commands Page](/docs/commands).\n\nThere is going to be a page for features and commands will be compiled there for context. Sorry if the commands page is a little unorganized.\n\n## Troubleshooting\n\nIn order to get the best support, please join the Discord Server and ask for help in the #help channel.\n\n- [Join Discord Server](/discord) \n\n## Thank you\n\n- [Support Ferris on Patreon](/patreon) \n\n- [Join Discord Server](/discord)\n\nThank you for taking the time to learn about how to use Ferris. We hope it helps your community!";
 	return (
 		<DocsLayout sidebar={<DocsHomeSidebar />}>
 			<Markdown data={data} />
