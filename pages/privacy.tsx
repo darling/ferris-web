9b9fb01930283724a736e5671f0666fab36ec6e2
Carter Black <carterb@mailbox.org>
2021-07-04 15:01:11 -0700

    prep for terms and privacy change

--- pages/privacy.tsx
+++ pages/privacy.tsx
@@ -6,47 +6,25 @@ import unified from 'unified';
 
 import Layout from '../components/Layout';
 
-const components: { [key: string]: FC } = {
-	p: (p) => <p className="py-2 text-gray-300">{p.children}</p>,
-	a: (p: any) => (
-		<a className="text-green-300 underline" href={p.href}>
-			{p.children}
-		</a>
-	),
-	ul: (p) => (
-		<ul className="list-disc list-inside py-4 text-gray-300">
-			{p.children}
-		</ul>
-	),
-	li: (p) => (
-		<li className="py-2 hover:text-gray-500 transition duration-200">
-			{p.children}
-		</li>
-	),
-	h2: (p) => <h2 className="text-xl text-gray-200 py-4">{p.children}</h2>,
-	h1: (p) => (
-		<h1 className="font-bold text-3xl text-white py-4">{p.children}</h1>
-	),
-};
-
 const Privacy = () => {
 	const data =
 		"# Privacy Policy for Ferris.gg\n\nAt Ferris.gg, accessible from [https://ferris.gg/](https://ferris.gg/), one of our main priorities is the privacy of our visitors. This Privacy Policy document contains types of information that is collected and recorded by Ferris.gg and how we use it.\n\nIf you have additional questions or require more information about our Privacy Policy, do not hesitate to contact us.\n\nEmail: [ferris@ey.lc](mailto:ferris@ey.lc)\n\n## General Data Protection Regulation (GDPR)\n\nWe are a Data Controller of your information.\n\nFerris.gg legal basis for collecting and using the personal information described in this Privacy Policy depends on the Personal Information we collect and the specific context in which we collect the information:\n\n*   Ferris.gg needs to perform a contract with you\n*   You have given Ferris.gg permission to do so\n*   Processing your personal information is in Ferris.gg legitimate interests\n*   Ferris.gg needs to comply with the law\n\nFerris.gg will retain your personal information only for as long as is necessary for the purposes set out in this Privacy Policy. We will retain and use your information to the extent necessary to comply with our legal obligations, resolve disputes, and enforce our policies.\n\nIf you are a resident of the European Economic Area (EEA), you have certain data protection rights. If you wish to be informed what Personal Information we hold about you and if you want it to be removed from our systems, please contact us.\n\nIn certain circumstances, you have the following data protection rights:\n\n*   The right to access, update or to delete the information we have on you.\n*   The right of rectification.\n*   The right to object.\n*   The right of restriction.\n*   The right to data portability\n*   The right to withdraw consent\n\n## Log Files\n\nFerris.gg follows a standard procedure of using log files. These files log visitors when they visit websites. All hosting companies do this and a part of hosting services' analytics. The information collected by log files include internet protocol (IP) addresses, browser type, Internet Service Provider (ISP), date and time stamp, referring/exit pages, and possibly the number of clicks. These are not linked to any information that is personally identifiable. The purpose of the information is for analyzing trends, administering the site, tracking users' movement on the website, and gathering demographic information.\n\n## Cookies and Web Beacons\n\nLike any other website, Ferris.gg uses 'cookies'. These cookies are used to store information including visitors' preferences, and the pages on the website that the visitor accessed or visited. The information is used to optimize the users' experience by customizing our web page content based on visitors' browser type and/or other information.\n\nFor more general information on cookies, please read [\"What Are Cookies\"](https://www.privacypolicyonline.com/what-are-cookies/).\n\n## Privacy Policies\n\nYou may consult this list to find the Privacy Policy for each of the advertising partners of Ferris.gg.\n\nThird-party ad servers or ad networks uses technologies like cookies, JavaScript, or Web Beacons that are used in their respective advertisements and links that appear on Ferris.gg, which are sent directly to users' browser. They automatically receive your IP address when this occurs. These technologies are used to measure the effectiveness of their advertising campaigns and/or to personalize the advertising content that you see on websites that you visit.\n\nNote that Ferris.gg has no access to or control over these cookies that are used by third-party advertisers.\n\n## Third Party Privacy Policies\n\nFerris.gg's Privacy Policy does not apply to other advertisers or websites. Thus, we are advising you to consult the respective Privacy Policies of these third-party ad servers for more detailed information. It may include their practices and instructions about how to opt-out of certain options.\n\nYou can choose to disable cookies through your individual browser options. To know more detailed information about cookie management with specific web browsers, it can be found at the browsers' respective websites.\n\n## Children's Information\n\nAnother part of our priority is adding protection for children while using the internet. We encourage parents and guardians to observe, participate in, and/or monitor and guide their online activity.\n\nFerris.gg does not knowingly collect any Personal Identifiable Information from children under the age of 13\\. If you think that your child provided this kind of information on our website, we strongly encourage you to contact us immediately and we will do our best efforts to promptly remove such information from our records.\n\n## Online Privacy Policy Only\n\nOur Privacy Policy applies only to our online activities and is valid for visitors to our website with regards to the information that they shared and/or collect in Ferris.gg. This policy is not applicable to any information collected offline or via channels other than this website.\n\n## Consent\n\nBy using our website, you hereby consent to our Privacy Policy and agree to its terms.";
 
 	return (
 		<Layout>
-			<div className="container px-4 lg:max-w-4xl mx-auto break-words">
-				{(unified()
-					.use(parse)
-					.use(rehtml)
-					.use(rereact, {
-						components: components,
-						Fragment: React.Fragment,
-						createElement: React.createElement,
-					})
-					.processSync(data).result as ReactElement) || (
-					<p>Error - Unable to parse</p>
-				)}
+			<div className="bg-white">
+				<div className="mx-auto prose py-20 px-4 md:px-0 prose-green">
+					{(unified()
+						.use(parse)
+						.use(rehtml)
+						.use(rereact, {
+							Fragment: React.Fragment,
+							createElement: React.createElement,
+						})
+						.processSync(data).result as ReactElement) || (
+						<p>Error - Unable to parse</p>
+					)}
+				</div>
 			</div>
 		</Layout>
 	);
--- pages/terms.tsx
+++ pages/terms.tsx
@@ -6,49 +6,28 @@ import unified from 'unified';
 
 import Layout from '../components/Layout';
 
-const components: { [key: string]: FC } = {
-	p: (p) => <p className="py-2 text-gray-300">{p.children}</p>,
-	a: (p: any) => (
-		<a className="text-green-300 underline" href={p.href}>
-			{p.children}
-		</a>
-	),
-	ul: (p) => (
-		<ul className="list-disc list-inside py-4 text-gray-300">
-			{p.children}
-		</ul>
-	),
-	li: (p) => (
-		<li className="py-2 hover:text-gray-500 transition duration-200">
-			{p.children}
-		</li>
-	),
-	h2: (p) => <h2 className="text-xl text-gray-200 py-4">{p.children}</h2>,
-	h1: (p) => (
-		<h1 className="font-bold text-3xl text-white py-4">{p.children}</h1>
-	),
-};
-
 const Terms = () => {
 	const data =
 		'# Terms and Conditions of Ferris.gg\n\nThe following terms and conditions (collectively, these "Terms and Conditions") apply to your use of [https://ferris.gg/](https://ferris.gg/), including any content, functionality and services offered on or via [https://ferris.gg/](https://ferris.gg/) (the "Website").\n\nPlease read the Terms and Conditions carefully before you start using [https://ferris.gg/](https://ferris.gg/), because by using the Website you accept and agree to be bound and abide by these Terms and Conditions.\n\nThese Terms and Conditions are effective as of Jun 28, 2021. We expressly reserve the right to change these Terms and Conditions from time to time without notice to you. You acknowledge and agree that it is your responsibility to review this Website and these Terms and Conditions from time to time and to familiarize yourself with any modifications. Your continued use of this Website after such modifications will constitute acknowledgement of the modified Terms and Conditions and agreement to abide and be bound by the modified Terms and Conditions.\n\n## Conduct on Website\n\nYour use of the Website is subject to all applicable laws and regulations, and you are solely responsible for the substance of your communications through the Website.\n\nBy posting information in or otherwise using any communications service, chat room, message board, newsgroup, software library, or other interactive service that may be available to you on or through this Website, you agree that you will not upload, share, post, or otherwise distribute or facilitate distribution of any content — including text, communications, software, images, sounds, data, or other information — that:\n\n*   Is unlawful, threatening, abusive, harassing, defamatory, libelous, deceptive, fraudulent, invasive of another\'s privacy, tortious, contains explicit or graphic descriptions or accounts of sexual acts (including but not limited to sexual language of a violent or threatening nature directed at another individual or group of individuals), or otherwise violates our rules or policies\n*   Victimizes, harasses, degrades, or intimidates an individual or group of individuals on the basis of religion, gender, sexual orientation, race, ethnicity, age, or disability\n*   Infringes on any patent, trademark, trade secret, copyright, right of publicity, or other proprietary right of any party\n*   Constitutes unauthorized or unsolicited advertising, junk or bulk email (also known as "spamming"), chain letters, any other form of unauthorized solicitation, or any form of lottery or gambling\n*   Contains software viruses or any other computer code, files, or programs that are designed or intended to disrupt, damage, or limit the functioning of any software, hardware, or telecommunications equipment or to damage or obtain unauthorized access to any data or other information of any third party\n*   Impersonates any person or entity, including any of our employees or representatives\n\nWe neither endorse nor assume any liability for the contents of any material uploaded or submitted by third party users of the Website. We generally do not pre-screen, monitor, or edit the content posted by users of communications services, chat rooms, message boards, newsgroups, software libraries, or other interactive services that may be available on or through this Website.\n\nHowever, we and our agents have the right at their sole discretion to remove any content that, in our judgment, does not comply with these Terms of Use and any other rules of user conduct for our Website, or is otherwise harmful, objectionable, or inaccurate. We are not responsible for any failure or delay in removing such content. You hereby consent to such removal and waive any claim against us arising out of such removal of content.\n\nYou agree that we may at any time, and at our sole discretion, terminate your membership, account, or other affiliation with our site without prior notice to you for violating any of the above provisions.\n\nIn addition, you acknowledge that we will cooperate fully with investigations of violations of systems or network security at other sites, including cooperating with law enforcement authorities in investigating suspected criminal violations.\n\n## Intellectual Property\n\nBy accepting these Terms and Conditions, you acknowledge and agree that all content presented to you on this Website is protected by copyrights, trademarks, service marks, patents or other proprietary rights and laws, and is the sole property of [https://ferris.gg/](https://ferris.gg/).\n\nYou are only permitted to use the content as expressly authorized by us or the specific content provider. Except for a single copy made for personal use only, you may not copy, reproduce, modify, republish, upload, post, transmit, or distribute any documents or information from this Website in any form or by any means without prior written permission from us or the specific content provider, and you are solely responsible for obtaining permission before reusing any copyrighted material that is available on this Website.\n\n## Third Party Websites\n\nThis Website may link you to other sites on the Internet or otherwise include references to information, documents, software, materials and/or services provided by other parties. These websites may contain information or material that some people may find inappropriate or offensive.\n\nThese other websites and parties are not under our control, and you acknowledge that we are not responsible for the accuracy, copyright compliance, legality, decency, or any other aspect of the content of such sites, nor are we responsible for errors or omissions in any references to other parties or their products and services. The inclusion of such a link or reference is provided merely as a convenience and does not imply endorsement of, or association with, the Website or party by us, or any warranty of any kind, either express or implied.\n\n## Disclaimer of Warranties, Limitations of Liability and Indemnification\n\nYour use of [https://ferris.gg/](https://ferris.gg/) is at your sole risk. The Website is provided "as is" and "as available". We disclaim all warranties of any kind, express or implied, including, without limitation, the warranties of merchantability, fitness for a particular purpose and non-infringement.\n\nWe are not liable for damages, direct or consequential, resulting from your use of the Website, and you agree to defend, indemnify and hold us harmless from any claims, losses, liability costs and expenses (including but not limites to attorney\'s fees) arising from your violation of any third-party\'s rights. You acknowledge that you have only a limited, non-exclusive, nontransferable license to use the Website. Because the Website is not error or bug free, you agree that you will use it carefully and avoid using it ways which might result in any loss of your or any third party\'s property or information.\n\n## Term and termination\n\nThis Terms and Conditions will become effective in relation to you when you create a [https://ferris.gg/](https://ferris.gg/) account or when you start using the [https://ferris.gg/](https://ferris.gg/) and will remain effective until terminated by you or by us.\n\n[https://ferris.gg/](https://ferris.gg/) reserves the right to terminate this Terms and Conditions or suspend your account at any time in case of unauthorized, or suspected unauthorized use of the Website whether in contravention of this Terms and Conditions or otherwise. If [https://ferris.gg/](https://ferris.gg/) terminates this Terms and Conditions, or suspends your account for any of the reasons set out in this section, [https://ferris.gg/](https://ferris.gg/) shall have no liability or responsibility to you.\n\n## Assignment\n\n[https://ferris.gg/](https://ferris.gg/) may assign this Terms and Conditions or any part of it without restrictions. You may not assign this Terms and Conditions or any part of it to any third party.\n\n## Governing Law\n\nThese Terms and Conditions and any dispute or claim arising out of, or related to them, shall be governed by and construed in accordance with the internal laws of the US without giving effect to any choice or conflict of law provision or rule.\n\nAny legal suit, action or proceeding arising out of, or related to, these Terms of Service or the Website shall be instituted exclusively in the federal courts of US.';
 
 	return (
 		<Layout>
-			<div className="container px-4 lg:max-w-4xl mx-auto break-words">
-				{(unified()
-					.use(parse)
-					.use(rehtml)
-					.use(rereact, {
-						components: components,
-						Fragment: React.Fragment,
-						createElement: React.createElement,
-					})
-					.processSync(data).result as ReactElement) || (
-					<p>Error - Unable to parse</p>
-				)}
+			<div className="bg-white">
+				<div className="mx-auto prose py-20 px-4 md:px-0 prose-green">
+					{(unified()
+						.use(parse)
+						.use(rehtml)
+						.use(rereact, {
+							Fragment: React.Fragment,
+							createElement: React.createElement,
+						})
+						.processSync(data).result as ReactElement) || (
+						<p>Error - Unable to parse</p>
+					)}
+				</div>
 			</div>
 		</Layout>
 	);
 };
+
 export default Terms;
