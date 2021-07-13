1f71fd01e4ae5f93e98f3882d6c8472191380d74
Carter Black <carterb@mailbox.org>
2021-07-13 01:29:53 -0700

    new login loading screen

--- pages/login.tsx
+++ pages/login.tsx
@@ -37,13 +37,26 @@ const Login = () => {
 
 	return (
 		<Layout>
-			<div className="container mx-auto">
-				<p className="animate-pulse">Logging in (please wait!!)</p>
-				{router.query.code ? null : (
+			<div className="bg-green-500">
+				<div className="max-w-2xl mx-auto text-center py-16 px-4 sm:py-20 sm:px-6 lg:px-8">
+					<h2 className="text-3xl font-extrabold text-white sm:text-4xl">
+						<span className="block animate-pulse">Logging in</span>
+					</h2>
+					<p className="mt-4 text-lg leading-6 text-green-200">
+						Please wait! If you're here too long, that might be a
+						problem! Contact{' '}
+						<span className="text-green-50">@Safe</span> and see
+						what's up!
+					</p>
 					<Link href="/api/login">
-						<a>Not getting redirected? Click here</a>
+						<a
+							hidden={!!!router.query.code}
+							className="mt-8 w-full inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-green-600 bg-white hover:bg-green-50 sm:w-auto"
+						>
+							Not getting redirected? Click here
+						</a>
 					</Link>
-				)}
+				</div>
 			</div>
 		</Layout>
 	);
